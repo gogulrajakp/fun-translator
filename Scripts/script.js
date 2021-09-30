cf34e218a39c42a6ed5e4263766fc3b92119cf00
gogulrajakp <gokulkp467@gmail.com>
2021-09-30 20:16:58 +0530

    updated styles

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,23 +3,30 @@
 const input = document.getElementById('input');
 const btnTranslate = document.getElementById('btn-translate');
 const output = document.getElementById('output');
+const selected = document.getElementById('languages');
+
+let languages = 'minion';
 
 const translateText = async function() {
   const inputText = input.value;
-  const language = 'minion';
+  console.log(input);
+  // const language = 'minion';
+
   if(inputText === '' || inputText === null){
     alert('Input text is mandatory')
   } else{
-    const url = `https://api.funtranslations.com/translate/${language}.json?text=${inputText}`;
+    const url = `https://api.funtranslations.com/translate/${languages}.json?text=${inputText}`;
 
+    console.log(url);
     const data = await fetch(url);
     const result = await data.json();
+    console.log(result);
     output.innerHTML = result.contents.translated;
   }
 }
 
 const checkKey = function(e){
-  console.log(e.key)
+  // console.log(e.key)
   if(e.key === 'Enter'){
     translateText();
   }
@@ -27,3 +34,10 @@ const checkKey = function(e){
 
 btnTranslate.addEventListener('click', translateText);
 document.addEventListener('keydown', checkKey);
+selected.addEventListener('change', e => {
+  languages = (e.target.value);
+  translateText();
+  console.log(languages)
+})
+
+console.log(languages);
